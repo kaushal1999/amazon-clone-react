57bb15d1d84d286e5399f8d83ffef1f5bca19dad
kaushal1999 <kaushalwaghela1999@gmail.com>
2023-06-10 15:30:59 +0530

    completed router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import  Home from './components/home'
+import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 class App extends Component {
   render() {
-    return <div className="app">Hello World!</div>;
+    return (
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/"><Home></Home></Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
---
+++ src/components/home.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Home() {
+   return <div className="home">Hello World Again!!</div>;    
+}
